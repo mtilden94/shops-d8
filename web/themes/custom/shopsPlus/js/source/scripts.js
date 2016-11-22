8b771bf13ebc132371c7af8da37a6885f2fb3ee3
Sergey Gridasov <sgridasov@danavero.com>
2016-11-22 12:02:15 +0200

    fix js; add font weights;

--- web/themes/custom/shopsPlus/js/source/scripts.js
+++ web/themes/custom/shopsPlus/js/source/scripts.js
@@ -35,76 +35,78 @@
 
  }(Drupal, jQuery, this));
  */
-jQuery(function ($) {
-    $(document).ready(function () {
-        //remove footer last link
-        var text_last_a_footer=$('#footer .menu--footer ul.menu li.menu-item:last-child a').text();
-        $('#footer .menu--footer ul.menu li.menu-item:last-child a').replaceWith("<span class='no-link'>"  + text_last_a_footer + "</span>" );
+(function (Drupal, $, window) {
+	jQuery(function ($) {
+		$(document).ready(function () {
+			//remove footer last link
+			var text_last_a_footer = $('#footer .menu--footer ul.menu li.menu-item:last-child a').text();
+			$('#footer .menu--footer ul.menu li.menu-item:last-child a').replaceWith("<span class='no-link'>" + text_last_a_footer + "</span>");
 
-        //Expanding Search box
-        //================================================================================
+			//Expanding Search box
+			//================================================================================
 
-        $('#block-shopsplus-search input.form-search').attr('placeholder', 'Search');
-        $('#block-shopsplus-search .form-actions .form-submit').on('click',
-            function (e) {
-                var this_form = $(this).parents('form:first');
-                var text_input = $('input.form-search', this_form).val();
-                if (this_form.hasClass('open-form')) {
-                    if (text_input == '') {
-                        e.preventDefault();
-                        this_form.removeClass('open-form');
-                    }
-                } else {
-                    e.preventDefault();
-                    this_form.addClass('open-form');
-                }
-            });
+			$('#block-shopsplus-search input.form-search').attr('placeholder', 'Search');
+			$('#block-shopsplus-search .form-actions .form-submit').on('click',
+				function (e) {
+					var this_form = $(this).parents('form:first');
+					var text_input = $('input.form-search', this_form).val();
+					if (this_form.hasClass('open-form')) {
+						if (text_input == '') {
+							e.preventDefault();
+							this_form.removeClass('open-form');
+						}
+					} else {
+						e.preventDefault();
+						this_form.addClass('open-form');
+					}
+				});
 
-        // hover main top menu
-        $('#header .menu--main > ul.menu > .menu-item--expanded').mouseenter(function () {
-            $('#header .menu--main li').removeClass('open-top-menu');
-            $(this).addClass('open-top-menu');
-        });
-        $('#header .menu--main > ul.menu > .menu-item--expanded').mouseleave(
-            function () {
-                $('#header .menu--main li').removeClass('open-top-menu');
-            }
-        );
+			// hover main top menu
+			$('#header .menu--main > ul.menu > .menu-item--expanded').mouseenter(function () {
+				$('#header .menu--main li').removeClass('open-top-menu');
+				$(this).addClass('open-top-menu');
+			});
+			$('#header .menu--main > ul.menu > .menu-item--expanded').mouseleave(
+				function () {
+					$('#header .menu--main li').removeClass('open-top-menu');
+				}
+			);
 
-        // mobile menu
-        $('#block-shopsplus-main-menu > ul.menu').clone().appendTo("#mobile-menu");
-        $('#mobile-menu .menu-item--expanded').append('<span class="prev-next"></span>');
-        $('#mobile-menu .pos-block').each(
-            function () {
-                $(this).children().first().prepend('<li class="beck">' + $(this).parent().children().first().text() + '</li>');
-            }
-        );
+			// mobile menu
+			$('#block-shopsplus-main-menu > ul.menu').clone().appendTo("#mobile-menu");
+			$('#mobile-menu .menu-item--expanded').append('<span class="prev-next"></span>');
+			$('#mobile-menu .pos-block').each(
+				function () {
+					$(this).children().first().prepend('<li class="beck">' + $(this).parent().children().first().text() + '</li>');
+				}
+			);
 
-        $('.mobile-menu-btn').on('click', function () {
-            $(this).parent().toggleClass('open-mobile-menu');
-        });
-        $('#mobile-menu .prev-next').on('click',
-            function () {
-                $(this).prev().addClass('right-none');
-                $(this).parent().addClass('pos-stat');
-            });
-        $('#mobile-menu .beck').on('click',
-            function () {
-                $(this).parent().parent().removeClass('right-none');
-                $(this).parents('li.menu-item--expanded').first().removeClass('pos-stat');
-            });
+			$('.mobile-menu-btn').on('click', function () {
+				$(this).parent().toggleClass('open-mobile-menu');
+			});
+			$('#mobile-menu .prev-next').on('click',
+				function () {
+					$(this).prev().addClass('right-none');
+					$(this).parent().addClass('pos-stat');
+				});
+			$('#mobile-menu .beck').on('click',
+				function () {
+					$(this).parent().parent().removeClass('right-none');
+					$(this).parents('li.menu-item--expanded').first().removeClass('pos-stat');
+				});
 
-        //Close on click outside
-        $(document).click(function (e) {
-            var target = e.target;
-            if (!$(target).is('#block-shopsplus-search input') && !$(target).is('#block-shopsplus-search .form-item')) {
-                $('#block-shopsplus-search form').removeClass('open-form');
-            }
-            if (!$(target).is('#mobile-menu *')) {
-                $('#mobile-menu').removeClass('open-mobile-menu');
-                $('#mobile-menu .pos-block').removeClass('right-none');
-                $('li.menu-item--expanded').removeClass('pos-stat');
-            }
-        });
-    });
-});
+			//Close on click outside
+			$(document).click(function (e) {
+				var target = e.target;
+				if (!$(target).is('#block-shopsplus-search input') && !$(target).is('#block-shopsplus-search .form-item')) {
+					$('#block-shopsplus-search form').removeClass('open-form');
+				}
+				if (!$(target).is('#mobile-menu *')) {
+					$('#mobile-menu').removeClass('open-mobile-menu');
+					$('#mobile-menu .pos-block').removeClass('right-none');
+					$('li.menu-item--expanded').removeClass('pos-stat');
+				}
+			});
+		});
+	});
+}(Drupal, jQuery, this));
