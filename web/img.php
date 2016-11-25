a48429abacab4e57f502b22b5be25d9533e9d56f
Sergey Gridasov <sgridasov@danavero.com>
2016-11-25 20:35:22 +0200

    remove contrib modules prefix

---
+++ web/img.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+<img srcset="http://abt-shop.dev/sites/default/files/styles/header_image_m/public/2016-11/Operations_Maintenance.jpg?itok=iDSwyDHT 320w, http://abt-shop.dev/sites/default/files/styles/header_image_t/public/2016-11/Operations_Maintenance.jpg?itok=80qmPFL_ 768w, http://abt-shop.dev/sites/default/files/styles/header_image_f/public/2016-11/Operations_Maintenance.jpg?itok=cLOYQMci 1200w" sizes="(min-width:1200px) 1200px, (min-width:768px) 768px, (min-width:320px) 320px, 320px" src="http://abt-shop.dev/sites/default/files/styles/header_image_m/public/2016-11/Operations_Maintenance.jpg?itok=iDSwyDHT" alt="jj">
+
+</body>
+</html>
