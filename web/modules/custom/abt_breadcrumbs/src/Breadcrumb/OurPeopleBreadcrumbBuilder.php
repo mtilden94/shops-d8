a9319fa272e938bfef235f2610334eb5385ebc40
Sergey Gridasov <sgridasov@danavero.com>
2016-12-09 19:51:12 +0200

    add patches ability; add <nolink> for menu items; make abt_breadcrumbs module with out people rule;

---
+++ web/modules/custom/abt_breadcrumbs/src/Breadcrumb/OurPeopleBreadcrumbBuilder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\abt_breadcrumbs\Breadcrumb;
+
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Link;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\node\NodeInterface;
+use Drupal\taxonomy\Entity\Term;
+
+class OurPeopleBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+  use StringTranslationTrait;
+
+
+  /**
+   * @inheritdoc
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    // This breadcrumb apply only for all custom node types
+    $parameters = $route_match->getParameters()->all();
+    if (isset($parameters['node'])) {
+      return $parameters['node']->getType() == 'our_people';
+    }
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function build(RouteMatchInterface $route_match) {
+    $breadcrumb = new Breadcrumb();
+    $breadcrumb->addLink(Link::createFromRoute($this->t('Home'), '<front>'));
+    $breadcrumb->addLink(Link::createFromRoute($this->t('Our people'), 'view.our_people.page_1'));
+
+    $parameters = $route_match->getParameters()->all();
+    if (!empty($parameters['node']->get('field_staff_type')->target_id)) {
+      $term = Term::load($parameters['node']->get('field_staff_type')->target_id);
+
+      $breadcrumb->addLink(Link::fromTextAndUrl($term->getName(), $term->toUrl()));
+    }
+    return $breadcrumb;
+  }
+}
