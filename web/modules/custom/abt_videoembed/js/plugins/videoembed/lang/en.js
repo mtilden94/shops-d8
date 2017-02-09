aa95ea489d6e0311d8adfa139bc8d5185aa9c2d5
Sameer Khan <skhan@bwm.com>
2017-02-09 11:37:49 -0500

    from server

---
+++ web/modules/custom/abt_videoembed/js/plugins/videoembed/lang/en.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * The abbr language string definitions.
+ */
+
+"use strict"
+
+CKEDITOR.plugins.setLang('videoembed', 'en', {
+    buttonTitle: 'Insert Abbreviation',
+    menuItemTitle: 'Edit Abbreviation',
+    dialogTitle: 'Video Embed Properties',
+    dialogVideoEmbedTitle: 'Video Embed Code',
+    dialogLinkText: 'Link Text',
+    dialogExplanationTitle: 'Explanation'
+});
